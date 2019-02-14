c6fb024f7c21633b0e13a91999d91ff76f94159f
jennytrieu115 <32472478+jennytrieu115@users.noreply.github.com>
2019-02-13 18:07:44 -0800

    update temp

--- temp.py
+++ temp.py
@@ -5,10 +5,12 @@ Spyder Editor
 This is a temporary script file.
 """
 # problem 1
+import matplotlib.pyplot as plt
 from random import randint
 import numpy as np
-def nSideDice(p):
-    side = randint(0, p - 1)
+
+def nSideDice(diceSideCount):
+    side = randint(0, diceSideCount - 1)
     return side
 
 def rollDice(diceSideCount, rollTimes):
@@ -19,12 +21,23 @@ def rollDice(diceSideCount, rollTimes):
     return rolls
 
 def validate(rolls, rollTimes):
+     
     for k in range(0, len(rolls)):
         rolls[k] = rolls[k] / rollTimes
     print("Sum = ", sum(rolls))
+    
 
-rollTimes = 100000
+rollTimes = 10000
 rolls = rollDice(6, rollTimes)
 print("Rolls = ", rolls)
 validate(rolls, rollTimes)
 
+# plotting
+#h1,binEdges = np.histogram(s,b)
+x = range(1,5,1)
+# Label 
+plt.xlabel('Number On The Face Of The Die')
+plt.ylabel('Probability')
+plt.title('PMF for an unfair N-side die')
+plt.xtick(b1)
+plt.show()
